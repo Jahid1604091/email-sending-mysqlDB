cb0583e6110b051bb5b0d48f9e7f34ea887ba33d
Jahid1604091 <jh409780@gmail.com>
2022-10-27 16:42:01 +0600

    html added to mail

--- routes/data.js
+++ routes/data.js
@@ -16,7 +16,18 @@ router.post('/', (req, res) => {
         }
         else {
             // sending mail
-            const message = `Name : ${req.body.name} \n\nProject_Details:${req.body.project_brief}`
+            const message = `<div> <img src='http://webgrafart.com/grafart%202-01.png' width='80' height='50'/>
+            <h3 style='color:indigo; border-bottom:1px solid indigo'>You have a notification</h3> \n\n
+            <div style='text-align:start;'>
+            
+            \n<p> <strong>Name</strong> :  ${req.body.name} </p>
+            \n<p><strong>Email</strong>  : ${req.body.email} </p>
+            \<p><strong>City</strong>  :  ${req.body.city}</p>
+            \n<p><strong>Budget</strong>  : : ${req.body.budget} </p>
+            \n<p><strong>Project Details</strong>  :  ${req.body.project_brief}</p>
+            </div>
+            </div>
+            `
 
             try {
                 await sendEmail({
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -19,7 +19,7 @@ async function sendEmail(options) {
     from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}> `, 
     to: options.email,
     subject: options.subject, 
-    text: options.message, 
+    html: options.message, 
   }
   const info = await transporter.sendMail(message);
 
